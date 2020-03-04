12433acda06b703591beaf110a3981d6ec5e0523
Abhay07 <abhaysrivastav007@gmail.com>
2020-03-04 13:35:50 +0530

    imageSlider

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,62 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a  and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div>
+  <h1>Image Slider</h1>
+   <div class="container">
+   <img  v-bind:src="currentImage">
+     
+   </div>
+ </div>
+  
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data :()=>({
+    imageList:["Tulips.jpg","Penguins.jpg","Lighthouse.jpg","Koala.jpg","Jellyfish.jpg"] ,
+    currentImage : "",
+    imageIndex : 0,
+
+  }),
+  created(){
+    this.currentImage = this.imageList[0]
+    this.slideImage()
+  },
+  methods:{
+    slideImage(){
+      var that = this
+
+       setInterval(function(){
+        if(that.imageIndex == that.imageList.length -1){
+          that.imageIndex = -1 
+        }
+        that.imageIndex = that.imageIndex + 1
+        that.currentImage = that.imageList[that.imageIndex]
+      },2000)
+    },
+
+    
+
+  },
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.container{
+  width: 700px;
+  border:1px solid black;
+
+  margin:auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+
+img{
+  height:100%;
+  width:100%;
+
+
+  
 }
 </style>
